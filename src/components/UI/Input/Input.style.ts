5c4b4bad49b517774aeb1f6456a4e8446b0980b8
Muhammadrashod Akromov <rashod0730@icloud.com>
2023-11-17 21:40:21 +0500

    not full

--- src/components/UI/Input/Input.style.ts
+++ src/components/UI/Input/Input.style.ts
@@ -20,7 +20,6 @@ export const StyledInput = styled.input`
   padding: 12px 15px;
   background-color:  ${(props) => props.theme.colors.bgc};
   border-radius: 10px;
-  border: 1px solid transparent;
 
   transition: 200ms;
 
